7f8138e1b22fd7fbee0558b31e93b49b764aa451
Matthew Casiro <mattcasiro@gmail.com>
2024-07-08 15:06:56 -0700

    Simplification

--- osx_setup.sh
+++ osx_setup.sh
@@ -1,3 +1,8 @@
+# Start from the home dir
+if [[ "$(pwd)" != "${HOME}" ]]; then
+	pushd ~ > /dev/null
+fi
+
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
@@ -10,21 +15,22 @@ chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install dotfiles
-pushd ~/src/MISC/dotfiles > /dev/null
-ln -sf ~/src/MISC/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/src/MISC/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/src/MISC/dotfiles/.aliases ~/.aliases
-ln -sf ~/src/MISC/dotfiles/.path ~/.path
-ln -sf ~/src/MISC/dotfiles/.benevity ~/.benevity
-ln -sf ~/src/MISC/dotfiles/.private ~/.private
-ln -sf ~/src/MISC/dotfiles/.env ~/.env
-ln -sf ~/src/MISC/dotfiles/.zshfuncs ~/.zshfuncs
-ln -sf ~/src/MISC/dotfiles/.gitignore_global ~/.gitignore_global
+pushd ~/src/personal/dotfiles > /dev/null
+ln -sf ~/src/personal/dotfiles/.aliases ~/.aliases
+ln -sf ~/src/personal/dotfiles/.awsrc ~/.awsrc
+ln -sf ~/src/personal/dotfiles/.benevity ~/.benevity
+ln -sf ~/src/personal/dotfiles/.env ~/.env
+ln -sf ~/src/personal/dotfiles/.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
+ln -sf ~/src/personal/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/src/personal/dotfiles/.zshfuncs ~/.zshfuncs
+ln -sf ~/src/personal/dotfiles/.zshrc ~/.zshrc
 popd > /dev/null # ~
 
+exec zsh
+
 # Install darcula theme for vim
-pushd ~/src/MISC > /dev/null
+pushd ~/src/personal > /dev/null
 git clone git@github.com:blueshirts/darcula.git
 mkdir -p ~/.vim/colors
 cp darcula/colors/darcula.vim ~/.vim/colors/
@@ -34,15 +40,16 @@ popd > /dev/null # ~
 mkdir -p ~/.vim/pack/plugins
 mkdir -p ~/.vim/pack/plugins/start
 mkdir -p ~/.vim/pack/plugins/opt
-# Terraform
+
+# Terraform for vim
 git clone https://github.com/hashivim/vim-terraform.git ~/.vim/pack/plugins/start/vim-terraform
 
 # Install powerline fonts
-pushd ~/src/MISC > /dev/null
+pushd ~/src/personal > /dev/null
 git clone https://github.com/powerline/fonts.git --depth=1
 pushd fonts > /dev/null
 ./install.sh
-popd > /dev/null # ~/src/MISC
+popd > /dev/null # ~/src/personal
 rm -rf fonts
 popd > /dev/null # ~
 
